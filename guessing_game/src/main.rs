d2c06a8e6fed273d117d0008625480c8071edca3
ernest <ernestzulpukarov@gmail.com>
2021-12-02 00:06:14 +0700

    C-2. Chapter 2 completed

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,28 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number\ninput you guess");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(value) => value,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => { println!("too small"); }
+            Ordering::Greater => { println!("too big"); }
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        };
+    };
 }
