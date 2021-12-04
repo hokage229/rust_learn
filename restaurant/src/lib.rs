26602568f6bb1584a8df4294f9fb03424cf2c1a0
ernest <ernestzulpukarov@gmail.com>
2021-12-04 17:10:48 +0700

    C-7. Chapter 7 completed

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,18 @@
+mod front_of_house;
+mod back_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    // front_of_house::hosting::add_to_waitlist();
+    // add_to_waitlist();
+
+    // let mut meal = back_of_house::Breakfast::summer("wheat");
+
+    // let order1 = Salad;
+    // let order1 = Soup;
+    // back_of_house::Appetizer::Soup;
+}
+
+fn serve_order() {}
+
